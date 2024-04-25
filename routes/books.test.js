817a7d1264af0abc634df1472165f24c4f2c9f77
Nicholas L <NLoo1@outlook.com>
2024-04-25 19:52:18 -0400

    Add tests

--- routes/books.test.js
+++ routes/books.test.js
@@ -3,6 +3,7 @@ const jsonschema = require("jsonschema");
 const app = require("../app");
 const db = require("../db");
 const Book = require("../models/book");
+const ExpressError = require("../expressError");
 
 describe("Books route test", function () {
   beforeEach(async function () {
@@ -33,6 +34,7 @@ describe("Books route test", function () {
         year: 2017,
       });
 
+      expect(response.statusCode).toEqual(201)
       expect(response.body).toEqual({
         book: {
           isbn: "0691161519",
@@ -91,6 +93,104 @@ describe("Books route test", function () {
       });
     });
   });
+
+  describe("GET /books by ISBN", function(){
+    test("Get book by ISBN", async function(){
+      let response = await request(app).get('/books/0691161518')
+
+      expect(response.body).toEqual({
+        book:{
+          isbn: "0691161518",
+          amazon_url: "http://a.co/eobPtX2",
+          author: "Test Author",
+          language: "test",
+          pages: 264,
+          publisher: "Test Publisher",
+          title: "Test-Up: Testing the Testing Tests in Testing Games",
+          year: 2017
+        }
+        
+      });
+    })
+
+    test("Fail to get book by non-existent ISBN", async function(){
+      let response = await request(app).get('/books/123456')
+
+      expect(response.statusCode).toBe(404)
+    })
+  })
+
+  describe("PUT /books by ISBN", function(){
+    test("Put book by ISBN", async function(){
+      let response = await request(app)
+    .put('/books/0691161518')
+    .send({
+        isbn: "0691161518",
+        amazon_url: "http://a.co/eobPtX2",
+        author: "Test Put",
+        language: "put",
+        pages: 264,
+        publisher: "put publisher",
+        title: "put title",
+        year: 2017
+    });
+
+
+      expect(response.body).toEqual({
+        book:{
+          isbn: "0691161518",
+          amazon_url: "http://a.co/eobPtX2",
+          author: "Test Put",
+          language: "put",
+          pages: 264,
+          publisher: "put publisher",
+          title: "put title",
+          year: 2017
+        }
+        
+      })
+    })
+
+    test("Fail to put book by non-existent ISBN", async function(){
+      let response = (await request(app)
+      .put('/books/123456')
+      .send({
+        author: "Test Put",
+        language: "put",
+        publisher: "put publisher",
+        title: "put title"
+      }))
+
+      expect(response.statusCode).toBe(400)
+    })
+
+    test("Fail to put book from bad JSON", async function(){
+      let response = (await request(app)
+      .put('/books/0691161518')
+      .send({
+        author: "Test Put",
+        language: "put",
+        publisher: "put publisher",
+        title: "put title"
+      }))
+
+      expect(response.statusCode).toBe(400)
+    })
+  })
+
+  describe("DELETE /books by ISBN", function(){
+    test("Delete book by ISBN", async function(){
+      let response = await request(app).delete('/books/0691161518')
+
+      expect(response.body).toEqual({ message: "Book deleted" })
+    })
+
+    test("Delete book with non-existent ISBN", async function(){
+      let response = await request(app).delete('/books/123456')
+
+      expect(response.statusCode).toBe(404)
+    })
+  })
 
   afterAll(async function () {
     await db.end();
