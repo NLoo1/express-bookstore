cad4a5768df37627d9bc80061bae6b59d649e965
Nicholas L <NLoo1@outlook.com>
2024-04-25 07:18:08 -0400

    Added bookSchema.js. Working on book tests

---
+++ routes/books.test.js
@@ -0,0 +1,53 @@
+const request = require("supertest");
+const jsonschema = require("jsonschema");
+
+const app = require("../app");
+const db = require("../db");
+const Book = require("../models/book");
+
+describe("Books route test", function() {
+
+    beforeEach(async function(){
+        await db.query("DELETE FROM books")
+
+        let book1 = await Book.create({
+            "isbn": "0691161518",
+            "amazon_url": "http://a.co/eobPtX2",
+            "author": "Test Author",
+            "language": "test",
+            "pages": 264,
+            "publisher": "Test Publisher",
+            "title": "Test-Up: Testing the Testing Tests in Testing Games",
+            "year": 2017
+          })
+    })
+
+
+    describe("Posting new books", function(){
+        test("Can post book", async function(){
+            let response = await request(app).post("/books/register")
+            .send({
+                "isbn": "0691161518",
+                "amazon_url": "http://a.co/eobPtX2",
+                "author": "Matthew Lane",
+                "language": "english",
+                "pages": 264,
+                "publisher": "Princeton University Press",
+                "title": "Power-Up: Unlocking the Hidden Mathematics in Video Games",
+                "year": 2017
+              })
+        })
+
+        expect(response.json.toEqual({
+            "isbn": "0691161518",
+            "amazon_url": "http://a.co/eobPtX2",
+            "author": "Matthew Lane",
+            "language": "english",
+            "pages": 264,
+            "publisher": "Princeton University Press",
+            "title": "Power-Up: Unlocking the Hidden Mathematics in Video Games",
+            "year": 2017
+        }))
+    })
+
+})
